b6a1b2ba434537a94d045a0c77c0b97eb2b79844
Darkce <darkce97@gmail.com>
2022-11-18 09:31:19 +0800

    build: add build tools

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2015,
+  },
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-useless-constructor': 'off',
+  },
+  ignorePatterns: ['/node_modules/', '/dist/', '/core/', 'website', '/src/bind/', 'vendors'],
+};
